eda7c7c7ca7de0f7032e48e0d82bac0f5929cec6
Bhaskar Kulshrestha <bhaskarkulshrestha03@gmail.com>
2023-10-01 16:09:01 +0530

    Firs t Commit

---
+++ build/precache-manifest.a16469472ed6097cb91bb3bc2ec46194.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e27e2032d5ba61ab31182d6536c2c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "722bc62b01dd9b6f1020",
+    "url": "/static/js/2.826d397e.chunk.js"
+  },
+  {
+    "revision": "6102d109fdbc99b21371",
+    "url": "/static/js/main.d23ef503.chunk.js"
+  },
+  {
+    "revision": "84832877ed76605d723e",
+    "url": "/static/js/runtime-main.aa743321.js"
+  }
+]);
